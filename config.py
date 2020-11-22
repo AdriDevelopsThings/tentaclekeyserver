d5ab73f3e07826db08f1139cc65bb2502bc159d0
AdriBloober <adrianbloober@gmail.com>
2020-11-22 22:21:13 +0000

    Add google analytics

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ REDIRECT_KEY_ADD_PATH = "/pks/add"
 SQLALCHEMY_PATH = "mysql+pymysql://tks:sajbashuifbhuiasbfhuiBASGIHJ@db/tks"
 MY_DOMAIN = "http://example.test"
 GPG_HOME_DIR = ".gnupg"
+GOOGLE_ANALYTICS_GTAG = None
--- resources/config.py
+++ resources/config.py
@@ -4,3 +4,4 @@ REDIRECT_KEY_ADD_PATH = "/pks/add"
 SQLALCHEMY_PATH = "mysql+pymysql://root@localhost/test"
 MY_DOMAIN = "http://example.test"
 GPG_HOME_DIR = ".gnupg"
+GOOGLE_ANALYTICS_GTAG = None
